931abc119539417880593dff8f05192997dccabe
Marcus Vieira <marcusbrunovieira@gmail.com>
2017-09-16 23:58:49 -0300

    Fetching books from the API and listing them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css';
 
 import Bookshelf from './components/Bookshelf';
 import SearchButton from './components/SearchButton';
 
 class BooksApp extends React.Component {
+  state = {
+    books: {
+      currentlyReading: [],
+      wantToRead: [],
+      read: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    BooksAPI.getAll().then((books) => { this.filterBooks(books) });
+  }
+
+  filterBooks(books) {
+    const currentlyReading = books.filter((book) => book.shelf == "currentlyReading")
+    const wantToRead = books.filter((book) => book.shelf == "wantToRead")
+    const read = books.filter((book) => book.shelf == "read")
+
+    this.setState({books: { currentlyReading, wantToRead, read }})
+  }
+
   render() {
     return (
       <div className="list-books">
@@ -14,15 +38,16 @@ class BooksApp extends React.Component {
         </div>
         <div className="list-books-content">
           <Bookshelf
-            books={[1,2,3,4]}
+            books={this.state.books.currentlyReading}
             title="Currently Reading" />
 
           <Bookshelf
             books={[1,2,3,4]}
+            books={this.state.books.wantToRead}
             title="Want to Read" />
 
           <Bookshelf
-            books={[1,2,3,4]}
+            books={this.state.books.read}
             title="Read" />
         </div>
 
